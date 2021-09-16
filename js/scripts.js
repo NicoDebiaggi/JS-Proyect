f7494af6efbb2dd84db6c86183a3dbbf121d40dc
Nicolas Enrique Debiaggi <nicodebiaggi@gmail.com>
2021-09-16 10:02:19 -0300

    mejora en la gestion de datos

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Scripts
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
 
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
@@ -26,12 +26,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-//form logic
-var name;
-var email;
-var money;
-var dues;
-var clientArr = [];
+//Form Logic
 var clients = $("#clients")
 var update = $("#update")
 clientArrStorage = JSON.parse(localStorage.getItem("listaClientes"));
@@ -39,32 +34,35 @@ if(clientArrStorage != null){
     clientArr = clientArrStorage
 }
 
-
-//function to select a word in an index from a string
+//Function to select a word in an index from a string
 function selectWord(str, index) {
     var strArr = str.split(" ");
     var selectedWord = strArr[index];
     return selectedWord;
 };
 
-//create class clients
+//Create class clients
 class Client{
-    constructor(){
+    constructor(name, email){
         this.name = name;
         this.email = email;
     }
 };
 
-//client register & due calc
-function registerAndCalc(){
-    name = $("#name").val();
-    email = $("#email").val();
-    money = $("#money").val();
-    dues = $("#dues");
-    currency = $("#currency");
+class CriptumBank{
+    constructor(){
+        this.name = $("#name").val();
+        this.email = $("#email").val();
+        this.money = parseInt($("#money").val());
+        this.clientArr = [];
+        this.interest = parseFloat($("#dues").val());
+        this.numberOfDues = parseInt(selectWord($("#dues").find(":selected").text(), 0));
+        this.currencyType = $("#currency").val();
+        this.currencySymbol = $("#currency").text();
+        this.emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+    }
 
-    
-    function getCurrencyValue(val) {
+    getCurrencyValue(val) {
         let valorDivisa = 0;
         $.ajax({
             method: "GET",
@@ -73,56 +71,56 @@ function registerAndCalc(){
             success: function(data) {
                 valorDivisa = data[val].casa.venta;
                 valorDivisa = parseFloat(valorDivisa.replace(/,/g, "."));
-                alert(`Hola ${name} el total a pagar por tu préstamo de seria de ${totalMoney}${currencySymbol} en ${numberOfDues} cuotas de ${dueFee}${currencySymbol}. El monto pesificado es de ${totalMoney * valorDivisa}ARS`)
+                localStorage.setItem("valorDolar", valorDivisa);
             }
+        }).done(() => {
+            valorDivisa = localStorage.getItem("valorDolar");
+            alert(`Hola ${this.name} el total a pagar por tu préstamo de seria de ${this.totalMoney}${this.currencySymbol} en ${this.numberOfDues} cuotas de ${this.dueFee}${this.currencySymbol}. El monto pesificado es de ${this.totalMoney * this.valorDivisa}ARS`)
         })
     }
 
-    //due & interest calc
-    let interest = dues.find(":selected").val();
-    let currencyType = currency.find(":selected").val();
-    let currencySymbol = currency.find(":selected").text();
-    let numberOfDues = parseInt(selectWord(dues.find(":selected").text(), 0));
-    let totalMoney = money*interest;
-    let dueFee = Math.round(totalMoney/numberOfDues*100)/100;
+    validateEmail(){
+        if (this.email.match(this.emailFormat)){
+
+
+            let totalMoney = Math.round(this.money*this.interest*100)/100;
+            let dueFee = Math.round(totalMoney/this.numberOfDues*100)/100;
+
+            if( this.name == null || this.name.length == 0 || /^\s+$/.test(this.name) ) {
+                alert("Nombre ingresado invalido!")
+            }
+            else{
+                if(this.currencyType == 1){
+                    this.getCurrencyValue(1)
+                    console.log(`Email ${this.email} enviado con el resumen`) 
+                }
+                else{
+                    alert(`Hola ${this.name} el total a pagar por tu préstamo de seria de ${totalMoney} en ${this.numberOfDues} cuotas de ${dueFee}`)
+                    console.log(`Email ${this.email} enviado con el resumen`)
+                }
     
-
-    //name & email validation
-    let emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
-
-    if (email.match(emailFormat)){
-        if( name == null || name.length == 0 || /^\s+$/.test(name) ) {
-            alert("Nombre ingresado invalido!")
+                //push valid clients to clientArr
+                if (this.clientArr.find(client => client.email == email)){
+                    console.log("Cliente registrado previamente")
+                }
+                else{
+                    this.clientArr.push(new Client(this.name, this.email));
+                    console.log(this.clientArr)
+                    localStorage.clear();
+                    localStorage.setItem("listaClientes", JSON.stringify(this.clientArr));
+                }
+            }
         }
         else{
-            if(currencyType == 1){
-                getCurrencyValue(1)
-                console.log(`Email ${email} enviado con el resumen`) 
-            }
-            else{
-                alert(`Hola ${name} el total a pagar por tu préstamo de seria de ${totalMoney} en ${numberOfDues} cuotas de ${dueFee}`)
-                console.log(`Email ${email} enviado con el resumen`)
-            }
-
-            //push valid clients to clientArr
-            if (clientArr.find(clientArr => clientArr.email == email)){
-                console.log("Cliente registrado previamente")
-            }
-            else{
-                clientArr.push(new Client());
-                console.log(clientArr)
-                localStorage.clear();
-                localStorage.setItem("listaClientes", JSON.stringify(clientArr));
-            }
+            alert("Email invalido!")
         }
-    }
-    else{
-        alert("Email invalido!")
     }
 }
 
+
 $("#button").on("click", () => {
-    registerAndCalc();
+    const criptumBank = new CriptumBank();
+    criptumBank.validateEmail();
 });
 $("#formID").on("keypress", function (e) {
     if(e.which == 13){
